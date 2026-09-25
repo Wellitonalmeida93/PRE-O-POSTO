083a57c4b264cb500467d798fa66077b72578a0e
Welliton Almeida <almeida.welliton@outlook.com>
2026-09-25 15:44:12 -0300

    Add additional email recipients to DESTINATARIOS

--- relatorio.py
+++ relatorio.py
@@ -22,7 +22,10 @@ REMETENTE_EMAIL = "welliton.almeida@pizzattolog.com.br"
 REMETENTE_SENHA = os.environ.get("SENHA_EMAIL")
 
 DESTINATARIOS = [
-    "welliton.almeida@pizzattolog.com.br"
+    "welliton.almeida@pizzattolog.com.br",
+    "Israel.joia@pizzattolog.com.br",
+    "daniel.sacramento@pizzattolog.com.br",
+    "magdo.ferreira@pizzattolog.com.br",
 ]
 
 SMTP_SERVIDOR = "smtp.gmail.com"
